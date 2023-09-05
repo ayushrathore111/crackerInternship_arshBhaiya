f554677e76471bc430d642a809a4ce1124f223cd
Ayush Rathore <94514710+ayushrathore111@users.noreply.github.com>
2023-09-05 13:45:13 +0530

    Update peak_element.cpp

--- peak_element.cpp
+++ peak_element.cpp
@@ -6,7 +6,7 @@
 // Output: 5
 // Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
 
-
+// approach 1:
 int findPeakElement(vector<int>& nums) {
         if(nums.size()<=1)return 0;
         if(nums.size()==2){
@@ -19,4 +19,38 @@ int findPeakElement(vector<int>& nums) {
         int val= *max_element(nums.begin(),nums.end());
         if(val==nums[nums.size()-1])return nums.size()-1;
         return 0;
+    }
+// approach 2:
+ int findPeakElement(vector<int>& nums) {
+    int n=nums.size();
+    if(n==1)
+    {
+      return 0;
+    } 
+    if(nums[0]>nums[1])
+    {
+      return 0;
+    }
+    if(nums[n-1]>nums[n-2])
+    {
+      return n-1;
+    }
+    int left=1;
+    int right=n-2;
+    while(left<=right)
+    {
+      int mid=(left+right)/2;
+      if(nums[mid]>nums[mid-1]&& nums[mid]>nums[mid+1])
+      {
+        return mid;
+      }
+      else if(nums[mid]>nums[mid-1])
+      {
+        left=mid+1;
+      }
+      else{
+        right=mid-1;
+      }
+    }
+return -1;
     }
